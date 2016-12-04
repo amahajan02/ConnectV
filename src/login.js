daa45c29af429ef9d999fca4dcc40464a5945f1d
amahajan02 <ashima.mahajan02@gmail.com>
2016-12-04 22:28:48 +0530

    Add files via upload

--- src/login.js
+++ src/login.js
@@ -1,1 +1,8 @@
-
+// $(document).ready(function () {
+// var submitBtn = document.getElementById("submitBtn");
+function submitBtnClicked() {
+	// TODO: Store username and password in database
+	console.log("-----------username=" + document.getElementById("userName").value);
+	console.log("-----------username=" + document.getElementById("pwd").value);
+}
+// });
