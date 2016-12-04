5ec737c213ed38951702088b926c06e8e7ea40ab
amahajan02 <ashima.mahajan02@gmail.com>
2016-12-04 23:23:47 +0530

    Add files via upload

--- src/login.js
+++ src/login.js
@@ -1,8 +1,17 @@
-// $(document).ready(function () {
-// var submitBtn = document.getElementById("submitBtn");
+var fs = require("fs");
 function submitBtnClicked() {
 	// TODO: Store username and password in database
 	console.log("-----------username=" + document.getElementById("userName").value);
 	console.log("-----------username=" + document.getElementById("pwd").value);
 }
-// });
+
+function createGroupForm() {
+	var loc = window.location.href;
+	console.log("-----------------loc=" + loc);
+	loc = loc.substr(0, loc.lastIndexOf("/") + 1)
+	location.replace(loc + "createGroup.html");
+}
+
+function createGroup() {
+	
+}
